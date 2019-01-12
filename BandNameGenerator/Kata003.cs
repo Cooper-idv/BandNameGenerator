066e0a41a51ca9fba3926f564a9855db82e0156d
Cooper Chen <cooper.chen@titansoft.com.sg>
2019-01-12 14:41:31 +0700

    My friend wants a new band name for her band. She like bands that use the formula: "The" + a noun with the first letter capitalized, for example:
    "dolphin" -> "The Dolphin"
    
    However, when a noun STARTS and ENDS with the same letter, she likes to repeat the noun twice and connect them together with the first and last letter, combined into one word (WITHOUT "The" in front), like this:
    
    "alaska" -> "Alaskalaska"
    
    Complete the function that takes a noun as a string, and returns her preferred band name written as a string.

---
+++ BandNameGenerator/Kata003.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandNameGenerator
+{
+    public class Kata003
+    {
+        public static string BandNameGenerator(string str)
+        {
+            string newBandName = "";
+            if (string.IsNullOrEmpty(str))
+            {
+                return newBandName;
+            }
+            if (str.Length < 2)
+            {
+                return newBandName.Insert(0,"The ").Insert(4, str.ToUpper());
+            }
+            if ( string.Compare(str.Substring(0,1), str.Substring(str.Length-1), true) == 0)
+            {
+                newBandName = string.Format("{0}{1}{1}", str.Substring(0, 1).ToUpper(), str.Substring(1));
+            }
+            else
+            {
+                newBandName = string.Format("{0}{1}{2}", "The ", str.Substring(0, 1).ToUpper(), str.Substring(1));
+            }
+
+            return newBandName;
+        }
+    }
+}
---
+++ BandNameGeneratorTests/Kata003Tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BandNameGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandNameGenerator.Tests
+{
+    [TestClass()]
+    public class Kata003Tests
+    {
+        [TestMethod()]
+        public void BandNameGeneratorTest()
+        {
+            Assert.AreEqual("", Kata003.BandNameGenerator(""));
+            Assert.AreEqual("The X", Kata003.BandNameGenerator("x"));
+            Assert.AreEqual("The Knife", Kata003.BandNameGenerator("knife"));
+            Assert.AreEqual("Tartart", Kata003.BandNameGenerator("tart"));
+            Assert.AreEqual("Sandlesandles", Kata003.BandNameGenerator("sandles"));
+            Assert.AreEqual("The Bed", Kata003.BandNameGenerator("bed"));
+        }
+    }
+}
